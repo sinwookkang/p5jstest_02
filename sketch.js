5b23cff1ad6aaf4752b7a02735a63132acda89a0
Sinwook Kang <siwg99kr@gmail.com>
2016-10-12 15:47:10 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(800,600);
+}
+
+function draw() {
+  background(0,255,255);
+  rect(250,250,100,100);
+  fill(255,0,0);
+  noStroke();
+  ellipse(100,100,125,75);
+}
